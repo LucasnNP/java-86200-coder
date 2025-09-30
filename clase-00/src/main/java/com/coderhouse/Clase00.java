10bc249e0cc898b217aa3890ab7dded21ededd41
lucas-prat-dev <pratlucas3@gmail.com>
2025-09-30 20:00:10 -0300

    agrgado de carpeta faltante

---
+++ clase-00/src/main/java/com/coderhouse/Clase00.java
@@ -0,0 +1,11 @@
+package com.coderhouse;
+
+public class Clase00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+
+	}
+
+}
